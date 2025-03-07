33e601a6e3d54b704ec2de7d2515a08850d17e80
djmjm <douglasjanovikmaliszewski@gmail.com>
2025-03-06 21:06:43 -0300

    feat(Person): Add validation inputs age, name.

--- src/main/java/com/example/model/Person.java
+++ src/main/java/com/example/model/Person.java
@@ -1,6 +1,8 @@
 package com.example.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,9 +17,11 @@ public class Person {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
     @Column(nullable = false)
     private String name;
 
+    @Min(value = 1)
     @Column(nullable = false)
     private int age;
 
