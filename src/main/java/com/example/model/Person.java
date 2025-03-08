f61f4760e04a2cc3f5d7786c2389a7fcceba8b2a
Douglas Janovik Maliszewski <douglasjanovikmaliszewski@gmail.com>
2025-03-08 13:16:31 -0300

    fix(person): change lombok construtor.

--- src/main/java/com/example/model/Person.java
+++ src/main/java/com/example/model/Person.java
@@ -4,53 +4,60 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.br.CPF;
 
 import java.util.UUID;
 
 @Entity
 @Table(name = "persons")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Person {
 
-    public Person() {
-    }
-    
     @Id
-    @GeneratedValue(strategy=GenerationType.UUID)
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     @NotBlank
-    @Column(nullable=false)
+    @Column(nullable = false)
     private String name;
 
-    @Min(value=1)
-    @Column(nullable=false)
+    @Min(1)
+    @Column(nullable = false)
     private int age;
 
     @CPF
-    @Column(nullable=false, unique=true)
+    @Column(nullable = false, unique = true)
     private String cpf;
 
     @Email
-    @Column(nullable=false, unique=true)
+    @Column(nullable = false, unique = true)
     private String email;
 
-
-    public UUID getId(){
-        return id;
+    
+    public Person() {
     }
-    public Person(Person original){
+
+    public Person(UUID id, String name, int age, String cpf, String email) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.cpf = cpf;
+        this.email = email;
+    }
+
+    public Person(Person original) {
         this.id = original.id;
         this.name = original.name;
         this.age = original.age;
-        this.email = original.email;
         this.cpf = original.cpf;
+        this.email = original.email;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
     }
 
     public String getName() {
